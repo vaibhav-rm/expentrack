a359d0b49f3d3a7106fc712da1788118f7dd6799
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-04-19 19:43:53 +0530

    added history model

--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import History
 # Create your views here.
 
+
 def index(request):
-    return render(request, "home/index.html")
+    if request.method == "POST":
+        data = request.POST
+        desc = data['desc']
+        amount = data['amount']
+        
+        History.objects.create(
+            desc=desc, 
+            amount=amount,
+        )
+        return redirect('/')
+    queryset = History.objects.all()
+    context = {'entries':queryset}
+    return render(request, "home/index.html", context)
